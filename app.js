2c5d9162d7069bb0ae092fa6c5f65b8ad4c1fc2a
Mauricio Minella <mauriciominella@gmail.com>
2016-01-09 17:27:31 -0200

    Books view created

--- app.js
+++ app.js
@@ -1,21 +1,59 @@
+//modules
 var express = require('express');
-
 var app = express();
 
+var port = process.env.PORT || 5000;
+
+var bookRouter = express.Router();
+
+//express setup
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 app.set('views', './src/views');
-app.set('view engine', 'handlebars');
+var handlebars = require('express-handlebars');
+app.engine('.hbs', handlebars({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+//routes
+var navigation = [{Link:'/Books', Text: 'Books'},{Link:'/Authors', Text:'Authors'}]
+
+var books = [
+	{
+		title: 'War and Peace',
+		genre: 'Historical Fiction',
+		author: 'Lev Nikolayevich Tolstoy',
+		read: false
+	},
+	{
+		title: 'Les Misérable',
+		genre: 'Historical Fiction',
+		author: 'Cool Author',
+		read: false
+	},
+	{
+		title: 'Other Book',
+		genre: 'Other Book Genre',
+		author: 'Other Book Author',
+		read: false
+	}
+];
+bookRouter.route('/')
+	.get(function(req, res){
+		res.render('books', {title:'Books', nav: navigation, books: books });
+	});
+
+bookRouter.route('/single')
+	.get(function (req, res) {
+		res.send('Hello Single Book');
+	});
+
+app.use('/Books', bookRouter)
 
 app.get('/', function(req, res){
-	res.render('index');
+	res.render('index', {	title: 'Home', nav: navigation });
 });
 
-app.get('/books', function(req, res){
-	res.send('Hello Books');
-});
-
-var port = 5000;
+//starting the server
 app.listen(5000, function(err){
 	console.log('running server on port ' + port);
 });
