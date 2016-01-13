5ae2809e35e196f74e6fd9bfc24c6c458180a07d
Mauricio Minella <mauricio.minella@bravi.com.br>
2016-01-13 17:17:48 -0200

    Auth routes

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 //modules
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var session = require('express-session');
+
 var app = express();
 
 var port = process.env.PORT || 5000;
 
-var navigation = [{Link:'/Books', Text: 'Book'},{Link:'/Authors', Text:'Author'}]
+var navigation = [{Link:'/Books', Text: 'Book'},{Link:'/Authors', Text:'Author'}];
 
 var bookRouter = require('./src/routes/bookRoutes')(navigation);
 var adminRouter = require('./src/routes/adminRoutes')(navigation);
+var authRouter = require('./src/routes/authRoutes')(navigation);
 
-//express setup
+//middleware
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(session({secret: 'library'}));
+require('./src/config/passport')(app);
+
 app.use(express.static('src/views'));
 app.set('views', './src/views');
+
 var handlebars = require('express-handlebars');
 app.engine('.hbs', handlebars({extname: '.hbs'}));
 app.set('view engine', '.hbs');
@@ -21,6 +34,7 @@ app.set('view engine', '.hbs');
 //routes
 app.use('/Books', bookRouter);
 app.use('/Admin', adminRouter);
+app.use('/Auth', authRouter);
 
 app.get('/', function(req, res){
 	res.render('index', {	title: 'Home', nav: navigation });
