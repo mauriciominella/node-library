1058455f88069a47002b81a1b22038fd262cf92b
Mauricio Minella <mauriciominella@gmail.com>
2016-01-13 22:33:10 -0200

    Passport configuration created. Also a new folder called strategies has been created under config in order to store passport strategies

--- src/config/passport.js
+++ src/config/passport.js
@@ -3,4 +3,14 @@ var passport = require('passport');
 module.exports = function (app) {
     app.use(passport.initialize());
     app.use(passport.session());
+
+  passport.serializeUser(function(user, done){
+      done(null, user);
+  });
+
+  passport.deserializeUser(function(userId, done){
+      done(null, user);
+  });
+
+  require('./strategies/local.strategy')();
 };
