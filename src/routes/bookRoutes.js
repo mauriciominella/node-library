bf3ce841d4b80409f463651a10a62dffbb9793d0
Mauricio Minella <mauricio.minella@bravi.com.br>
2016-01-19 17:10:01 -0200

    Controllers done. Next: Goodreads API

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -3,62 +3,18 @@ var bookRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
 var objectId = require('mongodb').ObjectID;
 
+
 var router = function(navigation){
 
-	bookRouter.use(function(req, res, next){
-			if(!req.user){
-					return res.redirect('/');
-			}
-			next();
-	});
+	var bookController = require('../controllers/bookController')(null, navigation);
 
+	bookRouter.use(bookController.middleware);
 
 	bookRouter.route('/')
-		.get(function(req, res){
-
-			var url = 'mongodb://localhost:27017/libraryApp';
-
-			mongodb.connect(url, function(err, db){
-					var collection = db.collection('books');
-
-					collection.find({}).toArray(
-						function(err, results){
-								res.render('bookListView',
-																			{
-																			 title:'Books',
-																			 nav: navigation,
-																			 books: results
-																		 });
-								db.close();
-					});
-
-			});
-
-
-		});
+		.get(bookController.getIndex);
 
 	bookRouter.route('/:id')
-		.get(function (req, res) {
-	    var id = new objectId(req.params.id);
-
-			var url = 'mongodb://localhost:27017/libraryApp';
-
-			mongodb.connect(url, function(err, db){
-					var collection = db.collection('books');
-
-					collection.findOne({_id: id},
-						function(err, results){
-								res.render('bookView',
-																			{
-																			 title:'Books',
-																			 nav: navigation,
-																			 book: results
-																		 });
-								db.close();
-					});
-
-			});
-		});
+		.get(bookController.getById);
 
 		return bookRouter;
 };
